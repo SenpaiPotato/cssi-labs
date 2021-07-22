c06335a286aae7a640acb3023d618fce99f77493
SenpaiPotato <minrulee8@gmail.com>
2021-07-22 22:06:18 +0000

    update with progress to medium goals

--- day09/starter/secretMessageApp/public/js/writeMessage.js
+++ day09/starter/secretMessageApp/public/js/writeMessage.js
@@ -1,3 +1,4 @@
+const maxChars = 10;
 const submitMessage = () => {
     console.log("submitting");
     const passcodeInput = document.querySelector('#passcode');
@@ -5,6 +6,15 @@ const submitMessage = () => {
     const passcodeValue = passcodeInput.value;
     const messageValue = messageInput.value;
 
+    if(!(/[A-Z]/.test(passcodeValue)) && !(/[0-9]/.test(passcodeValue))){
+        alert(`Passcode must contain at least 1 capital letter and 1 number`);
+        return;
+    }
+    if(messageValue.length >= maxChars){
+        alert(`Message must be less than ${maxChars} characters`);
+        return;
+    }
+   
     //Send to Firebase
     firebase.database().ref().push({
         message: messageValue,
